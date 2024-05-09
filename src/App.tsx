1fc3eeccb8abb1fcb165e5a5e438092eaf450334
Paavan Buddhdev <paavan.buddhdev@gmail.com>
2024-05-09 22:54:17 +0100

    lots yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import brid from './assets/bread.svg'
 import './App.css'
 import { MouseEventHandler, useState } from "react";
 // import { Drawer } from 'vaul'
@@ -11,33 +11,50 @@ import Intro from './Intro'
 const currentStep = 0;
 
 function App(props: { setStep: MouseEventHandler<HTMLButtonElement> | undefined; }) {
-  const [step, setStep] = useState(true); //step, setter, initial state
+  const [step, setStep] = useState(0); //step, setter, initial state
   
   function click() {
-    setStep(false); //on click, add 1 to step
+    setStep(step + 1); //on click, add 1 to step
   }
   
-  return (
-    <div className="max-w-screen-sm w-screen h-full bg-slate-900 flex justify-around flex-col mx-20 px-20">
-      {step ? (
-        <>
-          <Intro />
-          <div className="mx-4">
-            <Button variant="default" size="lg" className="w-full" onClick={click}>
-              Continue
-            </Button>
-          </div>
-        </>
-      ) : (
-        <>
+  let button = (<>
+    <div className="w-full">
+      <Button variant="default" size="lg" className="w-full" onClick={click}>
+        Continue
+      </Button>
+    </div>
+  </>)
+
+  let content;
+  if (step === 1) {
+    content = (
+      <>
+        <Intro />
+       
+      </>
+    );
+  } else if (step === 2) {
+    content = (
+      <>
         <Step1 />
-        <div className="mx-4">
-            <Button variant="default" size="lg" className="w-full" onClick={click}>
-              Continue
-            </Button>
-          </div>
-        </>
-      )}
+       
+      </>
+    );
+  } else {
+    content = (
+      <>
+      <h1 className="text-center text-3xl leading-[100px]">A<br/>Simple<br/>Loaf<br/></h1>
+      <div className="mx-auto">
+        <img src={brid} alt="" width="150"/>
+      </div>
+      </>
+    )
+  }
+
+  return (
+    <div className="max-w-screen-sm w-screen h-full bg-slate-950 flex flex-col justify-between align-middle mx-20 px-20 py-20">
+      {content}
+      {button}
     </div>
   );
 }
--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,35 +1,66 @@
-import './App.css'
-import { Drawer } from 'vaul'
-import { PlayCircle } from "lucide-react"
+import "./App.css";
+import { Drawer } from "vaul";
+import { PlayCircle } from "lucide-react";
 
 export default function Intro() {
-    // const [count, setCount] = useState(0)
-    
-    return (
-        <>
+  // const [count, setCount] = useState(0)
 
-        <h1 className="text-xl text-slate-50 mb-4">What you need:</h1>
-        <ul className="list-disc text-left">
-        <li className="text-xl text-slate-200">750g bread flour</li>
-        <li className="text-l text-slate-400 ml-4 list-none">+ a little extra for dusting</li>
-        <li className="text-xl text-slate-200">7g instant yeast</li>
-        <li className="text-xl text-slate-200">12g table salt</li>
-        <li className="text-xl text-slate-200">480ml water</li>
-        <li className="text-xl text-slate-200">25ml olive oil</li>
-        <li className="text-l text-slate-400 ml-4 list-none mb-4">+ a splash extra for the tin</li>
-        
-        <li className="text-xl text-slate-200">A large mixing bowl</li>
-        <li className="text-xl text-slate-200">A tea towel</li>
-        <li className="text-xl text-slate-200">A clean surface</li>
-        <li className="text-l text-slate-400 ml-4 list-none mb-4">for kneading on</li>
-        
-        <li className="text-xl text-slate-200">An oven</li>
-        <li className="text-xl text-slate-200">A large loaf tin</li>
-        <li className="text-l text-slate-400 ml-4 list-none mb-4">or similar casserole dish</li>
-        
-        <li className="text-xl text-slate-200">Dough scrapers</li>
-        <li className="text-l text-slate-400 ml-4 list-none mb-4">optional, but very useful!</li>
+  return (
+    <div>
+      <h1 className="text-2xl text-slate-50 mb-4">What you’ll need:</h1>
+
+      <div>
+        <div className="flex flex-row gap-2 mb-4 mt-8">
+          <h2 className="grow uppercase text-md tracking-wider">Ingredients</h2>
+          <hr className="bg-slate-100 h-[0.1rem] w-full my-auto" />
+        </div>
+        <ul className="list-disc text-left ml-8">
+          <li className="text-xl text-slate-200">750g bread flour</li>
+          <li className="text-l text-slate-400 ml-8 list-none">
+            + a little extra for dusting
+          </li>
+          <li className="text-xl text-slate-200">7g instant yeast</li>
+          <li className="text-xl text-slate-200">12g table salt</li>
+          <li className="text-xl text-slate-200">480ml water</li>
+          <li className="text-xl text-slate-200">25ml olive oil</li>
+          <li className="text-l text-slate-400 ml-8 list-none mb-4">
+            + a splash extra for the tin
+          </li>
         </ul>
-        </>
-    )
+      </div>
+
+      <div>
+        <div className="flex flex-row gap-2 mb-4 mt-8">
+          <h2 className="grow uppercase text-md tracking-wider">equipment</h2>
+          <hr className="bg-slate-100 h-[0.1rem] w-full my-auto" />
+        </div>
+        <ul className="list-disc text-left ml-8">
+          <li className="text-xl text-slate-200">A large mixing bowl</li>
+          <li className="text-xl text-slate-200">A tea towel</li>
+          <li className="text-xl text-slate-200">A large loaf tin</li>
+          <li className="text-l text-slate-400 ml-8 list-none">
+            or similar casserole dish
+          </li>
+          <li className="text-xl text-slate-200">Dough scrapers</li>
+          <li className="text-l text-slate-400 ml-8 list-none mb-4">
+            optional, but very useful!
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <div className="flex flex-row gap-2 mb-4 mt-8">
+          <h2 className="grow uppercase text-md tracking-wider">in&nbsp;ya&nbsp;kitchen</h2>
+          <hr className="bg-slate-100 h-[0.1rem] grow-0 w-full my-auto" />
+        </div>
+        <ul className="list-disc text-left ml-8">
+          <li className="text-xl text-slate-200">A clean surface</li>
+          <li className="text-l text-slate-400 ml-8 list-none">
+            for kneading on
+          </li>
+          <li className="text-xl text-slate-200">An oven</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
